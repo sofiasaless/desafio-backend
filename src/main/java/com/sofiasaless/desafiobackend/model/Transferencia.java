3fbfe363e787426a253bd81a2c0ffdcb2a782a98
sofiasaless <sofiasaleswk@gmail.com>
2024-12-30 00:09:46 -0300

    feat: iniciando entidade, useCase e controller para transferências

---
+++ src/main/java/com/sofiasaless/desafiobackend/model/Transferencia.java
@@ -0,0 +1,39 @@
+package com.sofiasaless.desafiobackend.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "transferencias")
+@Data
+public class Transferencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double valor;
+
+    @ManyToOne
+    @JoinColumn(name = "pagadorId", insertable = false, updatable = false)
+    private Usuario pagador;
+    
+    @ManyToOne
+    @JoinColumn(name = "beneficiarioId", insertable = false, updatable = false)
+    private Lojista beneficiario;
+
+    private Long pagadorId;
+    private Long beneficiarioId;
+
+    @CreationTimestamp
+    private LocalDateTime dataDeCriacao;
+}
---
+++ src/main/java/com/sofiasaless/desafiobackend/useCase/TransferirValorUseCase.java
@@ -0,0 +1,29 @@
+package com.sofiasaless.desafiobackend.useCase;
+
+import org.springframework.stereotype.Service;
+
+import com.sofiasaless.desafiobackend.model.Transferencia;
+import com.sofiasaless.desafiobackend.repository.LojistaRepository;
+import com.sofiasaless.desafiobackend.repository.UsuarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TransferirValorUseCase {
+    
+    private UsuarioRepository usuarioRepository;
+
+    private LojistaRepository lojistaRepository;
+
+    public Transferencia efetuarTransferencia() {
+        // validar se os usuarios passados existem e são válidos (ex: usuario tentando transferir para ele mesmo)
+        
+        
+        // validar se o pagador tem saldo suficiente para transferir
+
+        
+        return null;
+    }
+
+}
